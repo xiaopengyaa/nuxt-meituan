07bd08d3e8d490919d6f19416b646c28d2c61390
xiaopengyaa <857763476@qq.com>
2020-11-29 15:53:22 +0800

    静态资源设置

---
+++ components/public/header/geo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="m-geo">
+    <i class="el-icon-location"></i>
+    广州
+    <nuxt-link class="change-city" to="/changeCity">切换城市</nuxt-link>
+    [佛山 顺德 南沙]
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
---
+++ components/public/header/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-row class="m-header" :gutter="15">
+    <el-col :span="6">
+      <geo></geo>
+    </el-col>
+    <el-col :span="6"></el-col>
+    <el-col :span="6"></el-col>
+  </el-row>
+</template>
+
+<script>
+import Geo from './geo'
+export default {
+  components: {
+    Geo,
+  },
+}
+</script>
+
+<style></style>
